e9f08a302dbee507be255b1500030993ae1f1857
Alessandro Pace <alexpace6@gmail.com>
2022-05-06 14:46:30 +0200

    feature: Project created

---
+++ GNBSwiftUI/GNBSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GNBSwiftUI
+//
+//  Created by Alessandro Pace on 6/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GNBSwiftUI/GNBSwiftUI/GNBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNBSwiftUIApp.swift
+//  GNBSwiftUI
+//
+//  Created by Alessandro Pace on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct GNBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
